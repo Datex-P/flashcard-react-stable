f7bff66c2807cdba44fec02c47195effa3932324
Datex-P <fabian.weiss@outlook.com>
2022-06-08 12:43:03 +0200

    commented out google-sign in for heroku build

--- src/Login/LoginWithSignUp/Google.js
+++ src/Login/LoginWithSignUp/Google.js
@@ -13,7 +13,9 @@ function Google () {
 
   return (
     <div className='login__google posRelative overflowHidden'>
-     <div className="g-signin2" style={{width:'200px', height:'100px', background: 'green'}} onClick={onSignIn} dataOnsuccess="onSignIn" ></div> 
+     <div className="g-signin2" style={{width:'200px', height:'100px', background: 'green'}} 
+     //onClick={onSignIn} 
+     dataOnsuccess="onSignIn" ></div> 
        {/* <GoogleLogin
           clientId=""
           // render={renderProps => (
