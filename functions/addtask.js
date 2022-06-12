348f15670b234ddd93db1c5337798e87c1d1865d
Datex-P <fabian.weiss@outlook.com>
2022-06-12 14:26:47 +0200

    update addtask

--- functions/addtask.js
+++ functions/addtask.js
@@ -1,47 +1,30 @@
-
 const mongoose = require('mongoose')
-const Connection  = require('mongoose')
-const dbState = require('../server/dbstates.js')
-const User = require('../server/models/user')
-const fetch = require('node-fetch');
-const { MongoClient } = require('mongodb');
-const mongodb = require("mongodb");
-
 
 let uri = 'mongodb+srv://mongo123:mongo123@cluster0.m0wvo.mongodb.net/flashcard?retryWrites=true&w=majority'
 
-let client = new MongoClient(`${uri}`, {
+let client = mongoose.connect(`${uri}`, {
   useNewUrlParser: true
-},() => {
-  const state = Number(mongoose.connection.readyState);
-  console.log(dbState.find(f => f.value === state).label, 'to db'); // connected to db
-}
-);
+});
 
 const clientPromise = client.connect()
 
-
-
-
 exports.handler = async (event, context, callback) => {
  
-  context.callbackWaitsForEmptyEventLoop = false;
-  
-  //const MongoClient = mongodb.MongoClient;
-  
- 
+  context.callbackWaitsForEmptyEventLoop = false;  
   
   try {    
-   // const clientPromise = await client.connect()
-
     client = await clientPromise;
-    client.db('flashcards').createCollection('hifrommonday')
+    client.db('flashcards').collection('hifrommonday').insertOne({
+      item: 'canvas',
+      qty: 100,
+      tags: ['cotton'],
+      size: { h: 28, w: 35.5, uom: 'cm' }
+    });
 
     return {
       statusCode: 200,
     };
   } catch (err) {
-    console.log(err); // output to netlify function log
     return {
       statusCode: 500,
       body: JSON.stringify({ msg: err.message }) 
