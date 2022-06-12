42146f7d53b8c1749682faf999e9d6e6ab0af4aa
Datex-P <fabian.weiss@outlook.com>
2022-06-12 16:41:17 +0200

    update

--- functions/addtask.js
+++ functions/addtask.js
@@ -1,9 +1,15 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
+const mongoose = require('mongoose')
 
 
-let client = new MongoClient(`${process.env.MONGO_URI}`, {
-  useNewUrlParser: true
+// let client = new MongoClient(`${process.env.MONGO_URI}`, {
+//   useNewUrlParser: true
+// }
+// );
+let client = await mongoose.connect(`${process.env.MONGO_URI}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
 }
 );
 
