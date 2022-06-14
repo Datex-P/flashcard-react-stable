ca0b79bd377cee4f5024828f5cd3ba61f150bba4
Datex-P <fabian.weiss@outlook.com>
2022-06-14 09:31:04 +0200

    changes to addtask

--- functions/addtask.js
+++ functions/addtask.js
@@ -1,11 +1,11 @@
-//const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const mongoose = require('mongoose')
 const User = require('../server/models/user')
 
 exports.handler = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  
+  console.log(event, 'event')
+  console.log(event.body.name, 'name here')
    await mongoose.connect(`${process.env.MONGO_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
