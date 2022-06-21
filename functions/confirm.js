97afb73a74079690db42d4b04f713d0df0f9a1bb
Datex-P <fabian.weiss@outlook.com>
2022-06-21 09:27:50 +0200

    update

--- functions/confirm.js
+++ functions/confirm.js
@@ -8,27 +8,30 @@ exports.handler = async (event, context, callback) => {
    let url = event.path
    let index = url.indexOf("=");
    let token = url.slice(index+1)
-   console.log(token, 'token here')
-  const decoded = jwt.verify(token, process.env.SECRET);
 
-  console.log('confirm registration route triggered',decoded)
+   return {
+    statusCode: 400,
+    body: token
+}
+
+//    console.log(token, 'token here')
+//   const decoded = jwt.verify(token, process.env.SECRET);
+
+//   console.log('confirm registration route triggered',decoded)
   
-  if (decoded) {
-    const { email } = decoded;
-    console.log(decoded, 'decoded here')
+//   if (decoded) {
+//     const { email } = decoded;
+//     console.log(decoded, 'decoded here')
 
-    User.findOneAndUpdate({email: email}, {verified: true },(...e)=>{
-      console.log(e)
-    });
-  } else {
-    console.log('could not update user')
-    //redirect user to page with message about email confirmation link expiration
-    //and proposal to register again
-  }
+//     User.findOneAndUpdate({email: email}, {verified: true },(...e)=>{
+//       console.log(e)
+//     });
+//   } else {
+//     console.log('could not update user')
+//     //redirect user to page with message about email confirmation link expiration
+//     //and proposal to register again
+//   }
 
-    console.log('confirm registration got invoked')
-    return {
-        statusCode: 400,
-        body: "Oops"
-    }
+//     console.log('confirm registration got invoked')
+  
 };
